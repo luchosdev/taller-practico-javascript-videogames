9cfdfb4f810d4e194876ae1248b46faa8df260a8
Luis Sequera <luissequeracontreras@gmail.com>
2023-04-09 16:11:18 -0500

    clase 3-6

--- game.js
+++ game.js
@@ -1,10 +1,38 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
+
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
+
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
 
 function startGame() {
-  game.fillRect(0, 50, 100, 100);
-  //   game.clearRect(0, 0, 50, 50);
-  // game.clearRect(0, 0, 50, 50);
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  const map = maps[0];
+  const mapRows = maps[0].trim().split('\n');
+  const mapRowCols = mapRows.map((row) => row.trim().split(''));
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(emojis[mapRowCols[row - 1][col - 1]], elementsSize * col + 15, elementsSize * row - 10);
+    }
+  }
 }
