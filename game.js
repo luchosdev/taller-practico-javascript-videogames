94b7278d1675d7306bb80f75042ed9c0c6e1b5b8
Luis Sequera <luissequeracontreras@gmail.com>
2023-04-10 19:10:43 -0500

    clase 17

--- game.js
+++ game.js
@@ -5,11 +5,16 @@ const btnLeft = document.querySelector('#left');
 const btnRight = document.querySelector('#right');
 const btnDown = document.querySelector('#down');
 const spanLives = document.querySelector('#lives');
+const spanTime = document.querySelector('#time');
 
 let canvasSize;
 let elementsSize;
 let level = 0;
 let lives = 3;
+
+let timeStart;
+let timePlayer;
+let timeInterval;
 
 const playerPosition = {
   x: undefined,
@@ -50,6 +55,11 @@ function startGame() {
   if (!map) {
     gameWin();
     return;
+  }
+
+  if (!timeStart) {
+    timeStart = Date.now();
+    timeInterval = setInterval(showTime, 100);
   }
 
   const mapRows = map.trim().split('\n');
@@ -119,9 +129,10 @@ function levelFail() {
   console.log('Chocaste contra un enemigo! :(');
   lives--;
 
-    if (lives <= 0) {
+  if (lives <= 0) {
     level = 0;
     lives = 3;
+    timeStart = undefined;
   }
 
   playerPosition.x = undefined;
@@ -131,13 +142,18 @@ function levelFail() {
 
 function gameWin() {
   console.log('Terminaste el juego');
+  clearInterval(timeInterval);
 }
 
 function showLives() {
-  const heartsArray = Array(lives).fill(emojis["HEART"]);
+  const heartsArray = Array(lives).fill(emojis['HEART']);
 
-  spanLives.innerHTML = "";
-  heartsArray.forEach(heart => spanLives.append(heart));
+  spanLives.innerHTML = '';
+  heartsArray.forEach((heart) => spanLives.append(heart));
+}
+
+function showTime() {
+  spanTime.innerHTML = Date.now() - timeStart;
 }
 
 window.addEventListener('keydown', moveByKeys);
